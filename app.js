bbefda2dfb7ac90970b41c11a9b974ddc5bb13be
tonyishangu <tonyishangu@gmail.com>
2023-03-23 16:32:53 +0300

    handling get request

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const fs = require('fs')
 const express = require('express')
-
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello from the server side', app: 'natours'})
-})
-app.post('/', (req, res) => {
-    res.send('You can send data to this url')
+// const tours = fs.readFileSync(`__dirname`)
+const tours = JSON.parse(fs.readFileSync(`${ __dirname}/dev-data/data/tours-simple.json`))
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'Hello from the server side', app: 'natours'})
+// })
+// app.post('/', (req, res) => {
+//     res.send('You can send data to this url')
+// })
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 })
 
 const port = 3000
