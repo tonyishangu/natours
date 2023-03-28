3e1ce0ba5dc4b11ef0aa69cf22e3da0ac1eb0333
tonyishangu <tonyishangu@gmail.com>
2023-03-28 13:25:34 +0300

    create update api

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
     const tour = tours.find(el => el.id === id)
     // if(id > tours.length){
     if(!tour){
-        return res.status(400).json({
+        return res.status(404).json({
             status: 'fail',
             message: 'Invalid ID'
         })
@@ -54,6 +54,21 @@ app.get('/api/v1/tours/:id', (req, res) => {
         status: 'success',
         data: {
             tour: tour
+        }
+    })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+    if((req.params.id * 1) > tours.length){
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'updated tour'
         }
     })
 })
